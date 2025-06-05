9f8da9523081572d30b7499f510530b568979d21
mattswart <m@swart.cc>
2025-06-05 00:42:52 -0500

    configuring

---
+++ src/components/_data.jsx
@@ -0,0 +1,15 @@
+export const meta = [
+    businessName: "",
+    phoneNumber: "",
+    logo: "",
+    logoAlt: "",
+    hours: [
+        { day: "Monday",    isOpen: false,  timeOpen: "00:00",  timeClose "00:00" },
+        { day: "Tuesday",   isOpen: true,   timeOpen: "00:00",  timeClose "00:00" },
+        { day: "Wednesday", isOpen: true,   timeOpen: "00:00",  timeClose "00:00" },
+        { day: "Thursday",  isOpen: true,   timeOpen: "00:00",  timeClose "00:00" },
+        { day: "Friday",    isOpen: true,   timeOpen: "00:00",  timeClose "00:00" },
+        { day: "Saturday",  isOpen: true,   timeOpen: "00:00",  timeClose "00:00" },
+        { day: "Sunday",    isOpen: true,   timeOpen: "00:00",  timeClose "00:00" }
+    ]
+]
